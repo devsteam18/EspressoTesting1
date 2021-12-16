978eb61d65ab4f1edef61a6430150c76df54c3ce
devsteam18 <95477868+devsteam18@users.noreply.github.com>
2021-12-16 12:02:48 +0530

    made chsnges in test activities

--- app/src/androidTest/java/com/example/espressotesting/ui/movie/MovieFragmentTestSuite.kt
+++ app/src/androidTest/java/com/example/espressotesting/ui/movie/MovieFragmentTestSuite.kt
@@ -1,9 +1,5 @@
 package com.example.espressotesting.ui.movie
 
-import com.codingwithmitch.espressouitestexamples.ui.movie.DirectorsFragmentTest
-import com.codingwithmitch.espressouitestexamples.ui.movie.MovieDetailFragmentTest
-import com.codingwithmitch.espressouitestexamples.ui.movie.MovieListFragmentTest
-import com.codingwithmitch.espressouitestexamples.ui.movie.StarActorsFragmentTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
